47192851be511324ff2a1efa2c9a2ec71c6c36a2
GuihCFerreira <guihhh231@gmail.com>
2025-01-12 20:51:09 -0300

    fix: add other routes to index router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { planetRouter } from "./planet-route";
+import { spaceshipRouter } from "./spaceship-route";
+import { starsystemRouter } from "./starsystem-route";
+import { characterRouter } from "./character-route";
 
 const router = Router();
 
 router.use("/planets", planetRouter);
+router.use("/spaceships", spaceshipRouter);
+router.use("/star-systems", starsystemRouter);
+router.use("/characters", characterRouter);
 
 export { router };
